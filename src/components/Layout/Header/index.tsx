0eb06f137af6ef2560a616bad63d170446f81cda
Josué López <jomalolep@gmail.com>
2024-03-28 10:45:39 -0600

    feature: change language

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -7,6 +7,9 @@ import {
   Button,
   useTheme,
   useMediaQuery,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
 } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
@@ -19,14 +22,19 @@ import { useTranslation } from "react-i18next";
 const Header = () => {
   const { user, token } = useSelector((state: RootState) => state.auth);
   const dispatch = useAppDispatch();
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const isAuthenticated = token !== null;
   const isAdmin = user?.role === "admin";
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up('md'));
+  const matches = useMediaQuery(theme.breakpoints.up("md"));
 
   const handleLogout = () => {
     dispatch(logout());
+  };
+
+  const handleChangeLanguage = (event: SelectChangeEvent) => {
+    const language = event.target.value;
+    i18n.changeLanguage(language);
   };
 
   return (
@@ -55,6 +63,16 @@ const Header = () => {
                   </Button>
                 )}
                 <Box sx={{ display: "flex", alignItems: "center", ml: 2 }}>
+                  <Select
+                    value={i18n.language}
+                    onChange={handleChangeLanguage}
+                    displayEmpty
+                    inputProps={{ "aria-label": "Without label" }}
+                    sx={{ color: "white", borderColor: "white" }}
+                  >
+                    <MenuItem value="en">EN</MenuItem>
+                    <MenuItem value="es">ES</MenuItem>
+                  </Select>
                   <AccountCircleIcon sx={{ mr: 1 }} />
                   <Typography variant="body1" noWrap>
                     {user?.name}
